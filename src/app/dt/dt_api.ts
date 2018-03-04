e716a049c21c8307353aac94777d518a746c38fd
Tor Mala <tor.mala@webstep.no>
2018-03-04 20:35:14 +0100

    Some minor adjustments, settings timeout to avoid errors inside ng-event

--- src/app/dt/dt_api.ts
+++ src/app/dt/dt_api.ts
@@ -3,17 +3,18 @@ import { EventSourcePolyfill } from 'ng-event-source';
 import { BASIC_AUTH, PROJECT_ID } from '../apikey';
 import { Event, EventData } from './dt_streamresult';
 
-const API_URL = 'https://api.disruptive-technologies.com/v2beta1/projects/';
-const API_URL_VS = 'https://api.disruptive-technologies.com/emulator/v2beta1/projects/';
+const API_PROTO = 'https://';
+const API_URL = 'api.disruptive-technologies.com/v2beta1/projects/';
+const API_URL_VS = 'api.disruptive-technologies.com/emulator/v2beta1/projects/';
 const API_DEVICES = '/devices?';
 const API_DEVICE = '/devices/';
-const API_STREAM = 'devices:stream?';
+const API_STREAM = '/devices:stream?';
 
 const API_PUBLISH = ':publish';
 const FILTER_LABEL_VS = 'label_filters=virtual-sensor'; // checks for the presence of the label "virtual-sensor"
 
-const DEVICE_URL = API_URL + PROJECT_ID + API_DEVICE;
-const STREAM_URL = API_URL + PROJECT_ID  + '/' + API_STREAM;
+const DEVICE_URL = API_PROTO + API_URL + PROJECT_ID + API_DEVICE;
+const STREAM_URL = API_PROTO + API_URL + PROJECT_ID  + API_STREAM;
 
 
 export class DtApi {
@@ -28,7 +29,7 @@ export class DtApi {
 
   public getDevice(device_id: string): any {
     const url = DEVICE_URL + device_id;
-    return this.http.get(url, {headers: this.getHeaders()})
+    return this.http.get(url, {headers: this.getHeadersAuth()})
       .map(res => res.json());
   }
 
@@ -45,10 +46,10 @@ export class DtApi {
       '&device_types=temperature';
 
     const url = STREAM_URL + event_types + '&' + device_types + '&' + device_ids;
-    this.es = new EventSourcePolyfill(url, {headers: { Authorization: BASIC_AUTH }});
+    this.es = new EventSourcePolyfill(url, this.getEventSourceInitDict() );
     DtApi.IS_LISTENING = true;
     DtApi.SHOW_LISTENING = true;
-    console.log('Starting listening on sensors ' + device_ids);
+    console.log('Starting listening on sensors');
 
     this.es.onmessage = (response) => {
       if (DtApi.SHOW_LISTENING) {
@@ -61,11 +62,13 @@ export class DtApi {
     };
 
     this.es.onopen = (a) => {
-      console.log('Something happened', a);
+      const strA = JSON.stringify(a, null, ' ');
+      console.log('onOpen\n:' + strA);
     };
 
     this.es.onerror = (e) => {
-      console.error('Got error from eventsource:', e);
+      const strErr = JSON.stringify(e, null, ' ');
+      // console.log('Got error from eventsource:\n' + strErr);
     };
 
   }
@@ -75,23 +78,22 @@ export class DtApi {
     console.log('Skipping listening on sensors (SHOW_LISTENING set to FALSE)');
   }
 
-  private getHeaders() {
+  private getEventSourceInitDict() {
+    const timeOut = 30 * 60 * 1000;
+    return {
+      withCredentials: false,
+      headers: { 'Authorization': BASIC_AUTH },
+      errorOnTimeout: false,
+      heartbeatTimeout: timeOut,
+      checkActivity: false,
+      connectionTimeout: timeOut
+    };
+  }
+
+  private getHeadersAuth() {
     const headers = new Headers();
     headers.append('Authorization', BASIC_AUTH);
     return headers;
-  }
-
-  private playSound(mp3file: string): void {
-    console.log('Trying to play a rooster-sound');
-    // if player exists - reuse it
-    let player: any = document.getElementById('soundplayer');
-    if ( ! player ) {
-      player = document.createElement('audio');
-      player.setAttribute('id', 'soundplayer');
-    }
-    player.setAttribute('src', '/app/sound/' + mp3file);
-    console.log('Player set to play <audio src="' + player.getAttribute('src') + '"></audio>');
-    player.play();
   }
 
   public getDeviceId(targetName: string) {
